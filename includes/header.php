01d92ffed22425e184dcb0c83dd9a07428c9eee8
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2021-06-02 00:09:24 +0200

    Variable page title

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="UTF-8">
-  <title>Home - DHBW Memes</title>
+  <title><?php echo $pageTitle ?> - DHBW Memes</title>
   <link rel="stylesheet" href="styles/global.css">
   <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png">
