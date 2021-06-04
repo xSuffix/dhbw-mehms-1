f76ae3fd4e955515e6570834ae3aaa3c8779afb5
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2021-06-04 21:19:06 +0200

    Add dark mode

--- includes/meta.php
+++ includes/meta.php
@@ -1,4 +1,5 @@
 <meta charset="UTF-8">
+<script type="text/javascript" src="/scripts/dark-mode.js"></script>
 <link rel="stylesheet" href="/styles/global.css">
 <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0">
 <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png">
