072ec931fb3b52143614dd28721552caae8b89f8
nillga <71821765+nillga@users.noreply.github.com>
2021-07-11 18:27:36 +0200

    Numbers page with subpages

---
+++ numbers-info-404.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+  <title>Math - DHBW Mehms</title>
+  <?php include("includes/meta.php"); ?>
+  <style>
+    :root {
+      --banner-top: #b167eb;
+      --banner-bottom: #4bd8f6;
+    }
+
+    .vines {
+      animation: 0.2s color-p-primary forwards;
+    }
+  </style>
+  <link rel="stylesheet" href="pog.css">
+</head>
+
+
+<body>
+  <?php include("includes/header.php"); ?>
+
+  <main class="container">
+  <div class="lulw">
+    <h1>404</h1>
+    <h5 style="margin-left: auto">Page not found</h5>
+  </div>
+  <a href="numbers.php" id="return"><div class="kek" style="padding-bottom: 0">
+    <div class="firstrow">
+      <h3><span class="left">Haha you should have expected this, return to ranking?</span></h3>
+    </div>
+  </div></a>
+  </main>
+
+  <?php include("includes/bottom-navigation.php"); ?>
+</body>
+
+</html>
---
+++ numbers-info-9.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+  <title>Math - DHBW Mehms</title>
+  <?php include("includes/meta.php"); ?>
+  <style>
+    :root {
+      --banner-top: #b167eb;
+      --banner-bottom: #4bd8f6;
+    }
+
+    .vines {
+      animation: 0.2s color-p-primary forwards;
+    }
+  </style>
+  <link rel="stylesheet" href="pog.css">
+</head>
+
+
+<body>
+  <?php include("includes/header.php"); ?>
+
+  <main class="container">
+  <div class="lulw">
+    <h1>9</h1>
+    <h5 style="margin-left: auto">A decimalist's greatness</h5>
+  </div>
+  <div class="kek" style="padding-bottom: 0">
+    <div class="firstrow">
+      <h3><span class="left">More information: 9</span></h3><hr>
+    </div>
+    <div class="text">
+      <p>The number '9' has always received a lot of fascination, mostly as a part of '19' or even '99' and their use in literature and so on. 
+        Great examples are the heavy use of 9ish numbers in the complete works of the author Stephen King but also the 9 lives of a cat or 9 months 
+        of pregnancy or even Dante's 9 circles of hell. <br>
+        As the square of '3' it bears as well the trinity of trinities with a great example of a popcultural example being the three Star Wars 
+        trilogies. The most interesting part about the number '9' results of our use of the decimal system: it is the biggest single digit numbers 
+        and represents incomplete magnitude or some form of final infinity.
+      </p>
+  </div>
+  </main>
+
+  <?php include("includes/bottom-navigation.php"); ?>
+</body>
+
+</html>
