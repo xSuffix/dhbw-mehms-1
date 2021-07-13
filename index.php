16affa2f9cb6bd0cf86a4df145e27488ab11e6ee
Jan <jan.froehlich.janf@gmail.com>
2021-07-13 19:27:07 +0200

    Major header changes
    In order to indroduce a search bar at the top of index, major header changes were necessary.

--- index.php
+++ index.php
@@ -13,11 +13,15 @@
 
   <main class="container">
 
-    <div class="toolbar">
-      <div class="searchbar">
-        <input type="text">
+    <div class="toolbar-wrapper">
+      <div class="toolbar">
+        <svg xmlns="http://www.w3.org/2000/svg" class="i-search" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+        </svg>
+        <input type="text" spellcheck="false" placeholder="Search Mehms">
       </div>
     </div>
+
 
     <div id="gallery" class="gallery">
       <?php $dirname = "./assets/memes/";
@@ -29,7 +33,7 @@
           try {
             echo '<a class="meme-card" style="width:' . $sizes[0] * 300 / $sizes[1] .
               'px; flex-grow: ' . $sizes[0] * 300 / $sizes[1] . '"><div style="padding-top: ' .
-              $sizes[1] / $sizes[0] * 100 . '%"></div><img src="' . $image . '" /></a>';
+              $sizes[1] / $sizes[0] * 100 . '%"></div><img src="' . $image . '" loading="lazy" /></a>';
           } catch (DivisionByZeroError $e) {
           }
         }
--- scripts/header-scroll.js
+++ scripts/header-scroll.js
@@ -1,11 +1,38 @@
 // Hide when user scrolls down, show when user scrolls up
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("page-header").style.top = "0";
-  } else if (currentScrollPos > 32) {
-    document.getElementById("page-header").style.top = "-128px";
+
+document.addEventListener('DOMContentLoaded', function () {
+  var body = document.body;
+  var pageHeader = document.getElementById("header-wrapper");
+  var prevScrollPos = window.pageYOffset;
+  window.onscroll = function () {
+
+    var currentScrollPos = window.pageYOffset;
+
+    if (currentScrollPos > 80) {
+      body.classList.add("scrolled");
+    } else {
+      body.classList.remove("scrolled");
+    }
+
+    if (prevScrollPos > currentScrollPos) { // scroll up      
+      if (currentScrollPos > 96) {
+        body.classList.add("scroll-up")
+        pageHeader.style.top = "0";
+        body.classList.add("header-sm");
+      } else { // transition from fixed to absolute
+        if (currentScrollPos < 32) {
+          body.classList.remove("header-sm");
+          pageHeader.style.top = "-72px";
+        }
+      }
+
+    } else { // scroll down
+      body.classList.remove("scroll-up");
+      if (currentScrollPos > 96) {
+        body.classList.add("header-sm");
+        pageHeader.style.top = "-72px";
+      }
+    }
+    prevScrollPos = currentScrollPos;
   }
-  prevScrollpos = currentScrollPos;
-}
+});
