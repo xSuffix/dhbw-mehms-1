75b1521347df9b0ece7b05b0657674d9282b906b
nillga <71821765+nillga@users.noreply.github.com>
2021-07-20 21:25:59 +0200

    jetzt richtig

--- includes/numbers/404.php
+++ includes/numbers/404.php
@@ -4,7 +4,7 @@
     <h2>Page not found</h2>
   </div>
   <a href="zahlen" id="return">
-    <div class="paper" style="padding-bottom: 0">
+    <div class="paper">
       <div>
         <h3>Haha you should have expected this, return to ranking?</h3>
       </div>
