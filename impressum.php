3aaf536652c7921afe525e2bd7b5bf8faa9fbbea
I3lacksheep <85186296+I3lacksheep@users.noreply.github.com>
2021-07-20 21:34:00 +0200

    Datenschutz & Impressum

--- impressum.php
+++ impressum.php
@@ -3,7 +3,7 @@
 
 <head>
   <title>Impressum - DHBW Mehms</title>
-  <link href="styles/kontakt.css" rel="stylesheet">
+  <link href="styles/global.css" rel="stylesheet">
   <?php include("includes/meta.php"); ?>
   <style>
     :root {
@@ -18,29 +18,29 @@
   <?php include("includes/header.php"); ?>
 
   <main class="container">
-    <article>
-	<div><h1 class='document'>Impressum</h1><h3 class='document'>Angaben gemäß § 5 TMG</h3>
-	<p class='document'>DHBW Mosbach <br> 
+    <article class="paper">
+	<div><h1 >Impressum</h1><h3 >Angaben gemäß § 5 TMG</h3>
+	<p >DHBW Mosbach <br> 
 	Lohrtalweg 10 <br> 
 	74821 Mosbach 
 	</p>
-	<h3 class='document'>Vertreten durch:</h3>
+	<h3 >Vertreten durch:</h3>
 	<ol>
 		<li>2032766 (Jan)</li>
 		<li>2318815 (Gabriel)</li>
-		<li>1145498 (Miri)<br></li>
+		<li>1145498 (Miriam)<br></li>
 	</ol>
-	<h3 class='document'>Kontakt:</h3>
-	<p class='document'>Telefon: 97379-420034297<br>
-	E-Mail: <a class='document' href='mailto:mehms4DHBW@gmail.com'>mehms4DHBW@gmail.com</a></p>
+	<h3 >Kontakt:</h3>
+	<p >Telefon: 97379-420034297<br>
+	E-Mail: <a class="underline" href='mailto:mehms4DHBW@gmail.com'>mehms4DHBW@gmail.com</a></p>
 	
-	<h2 class='document'>Haftung für Inhalte</h2>
+	<h2 >Haftung für Inhalte</h2>
 	Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
 	 
  
-	<h2 class='document'> Datenschutz </h2>
-	<p class='document'>Beachten Sie bitte die verbindliche 
-	<a class='document'  href="datenschutz.php">Datenschutzrichtlinie</a> der DHBW Mehms.</p>
+	<h2 > Datenschutz </h2>
+	<p >Beachten Sie bitte die verbindliche 
+	<a class="underline" href="datenschutz.php">Datenschutzrichtlinie</a> der DHBW Mehms.</p>
 	
 	</div>
 	</article>
