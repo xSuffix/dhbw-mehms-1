8083d95882c15e31dc3046b48dd6525758c88924
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2021-07-20 11:30:22 +0200

    Improve Footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,21 +1,43 @@
-<footer class="container">
+<footer class="container md">
   <div class="container">
-    <h3 class="">DHBW Mehms</h3>
     <div>
+      <section class="about">
+        <p><strong>Über DHBW Mehms</strong></p>
+        <div class="logo">
+          <img src="./assets/logo.png" alt="Dogo the Logo" height="62">
+          <div>
+            <div>DHBW</div>
+            <div>Mehms</div>
+          </div>
+        </div>
+        <p>Diese Webseite ist eine gemeinsame Prüfungsleistung im Kurs Web-Engineering der DHBW Mosbach.</p>
+      </section>
+      <section>
+        <p><strong>Folge uns</strong></p>
+        <div class="social-icons">
+          <a href="https://github.com/xSuffix/dhbw-memes" target="_blank" rel="noopener">
+            <img src="./assets/icons/github.svg" alt="GitHub" height="32">
+          </a>
+        </div>
+      </section>
+      <section class="sitemap">
+      <p><strong>Sitemap</strong></p>
+        <p><a href="./">Mehms</a></p>
+        <p><a href="vines">Vines</a></p>
+        <p><a href="numbers">Zahlen</a></p>
+        <p><a href="kontakt">Kontakt</a></p>
+        <p><a href="mehm-einsendung">Mehm Einsendung</a></p>
+      </section>
+      <section class="sitemap">
+        <p><strong>Rechtliches</strong></p>
+        <p><a href="impressum">Impressum</a></p>
+        <p><a href="datenschutz">Datenschutz</a></p>
+      </section>
       <section>
         <p><strong>DHBW Mosbach</strong></p>
-        <p><span class="h-icon i-home"></span>Lohrtalweg 10<br>74821 Mosbach</p>
-        <p><span class="h-icon i-phone"></span>06261 / 939-504</p>
-        <p><span class="h-icon i-printer"></span>06261 / 939-0</p>
-      </section>
-      <section>
-        <p><strong>Entwickler</strong></p>
-        <p><a href="https://github.com/xSuffix/dhbw-memes">GitHub</a></p>
-      </section>
-      <section>
-        <p><strong>Rechtliches</strong></p>
-        <p><a href="impressum.php">Impressum</a></p>
-        <p><a href="datenschutz.php">Datenschutz</a></p>
+        <p><a href="https://goo.gl/maps/TvTZgwM64HwZpfdLA" target="_blank" rel="noopener"><span class="h-icon i-home"></span>Lohrtalweg 10<br>74821 Mosbach</a></p>
+        <p><a href="tel:+4962619390"><span class="h-icon i-phone"></span>06261 / 939-0</a></p>
+        <p><a href="fax:+4962619390"><span class="h-icon i-printer"></span>06261 / 939-504</a></p>
       </section>
     </div>
   </div>
--- kontakt.php
+++ kontakt.php
@@ -21,7 +21,7 @@
 <body>
 	<?php include("includes/header.php"); ?>
 
-	<main class="container">
+	<main class="container md">
 		<section class="contact">
 			<div>
 				<h1>Kontakt</h1>
